44e7fefe8cd97f814937ba22596d99bc577b6722
KongGuRE <dongjin0316@gmail.com>
2023-02-03 19:38:22 +0900

    feat: 이미지 관련 지원 함수, Mask 이미지 추적 함수 추가

---
+++ ImageAssist.cs
@@ -0,0 +1,53 @@
+﻿using OpenCvSharp;
+using System.Drawing;
+
+namespace ImageAssist
+{
+    public class ImageAssist
+    {
+        /// <summary>
+        /// 이미지 검색 확장자 필터 설정
+        /// </summary>
+        private static readonly string[] imageExts = new[] { ".jpg", ".bmp", ".png" }; // 검색할 확장자 필터
+
+        public static void SWImage(Mat img_contour)
+        {
+            Cv2.ImShow("Image_Contour", img_contour);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+        // ExtractCoordinatesMaskImage
+        // ImageAssistExtension
+
+        public static Bitmap LoadBitmap(string path)
+        {
+            if (imageExts.Contains(Path.GetExtension(path)))
+            {
+                if (File.Exists(path))
+                {
+                    // open file in read only mode
+                    using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        // get a binary reader for the file stream
+                        using (BinaryReader reader = new BinaryReader(stream))
+                        {
+                            // copy the content of the file into a memory stream
+                            var memoryStream = new MemoryStream(reader.ReadBytes((int)stream.Length));
+                            // make a new Bitmap object the owner of the MemoryStream
+                            return new Bitmap(memoryStream);
+                        }
+                    }
+                }
+                else
+                {
+                    // MessageBox.Show("Error Loading File.", "Error!", MessageBoxButtons.OK);
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ImageAssistExtension.cs
@@ -0,0 +1,123 @@
+﻿using OpenCvSharp;
+using System.Drawing;
+using Point = OpenCvSharp.Point;
+
+namespace ImageAssist
+{
+    public class ImageAssistExtension
+    {
+        public static (Point, Point)? ExtractCoordinatesMaskImage(string imPath)
+        {
+            using Mat matIm = Cv2.ImRead(imPath);
+            return ExtractCoordinatesMaskImage(matIm);
+        }
+
+        public static (Point, Point)? ExtractCoordinatesMaskImage(string imPath, out Mat? outMat)
+        {
+            using Mat matIm = Cv2.ImRead(imPath);
+            return ExtractCoordinatesMaskImage(matIm, out outMat);
+        }
+
+        public static (Point, Point)? ExtractCoordinatesMaskImage(Mat simRImage)
+        {
+            Mat simROIImage = new();
+            Cv2.InRange(simRImage, new Scalar(0, 0, 200), new Scalar(50, 50, 255), simROIImage);
+            Cv2.Threshold(simROIImage, simROIImage, 127, 255, ThresholdTypes.Binary);
+
+            Cv2.FindContours(simROIImage, out Point[][] contours, out _, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+            List<int> xList = new();
+            List<int> yList = new();
+
+            int p1X;
+            int p2X;
+            int p1Y;
+            int p2Y;
+            try
+            {
+                List<Point[]> new_contours = new();
+                foreach (Point[] p in contours)
+                {
+                    foreach (Point p2 in p)
+                    {
+                        xList.Add(p2.X);
+                        yList.Add(p2.Y);
+                    }
+
+                    double length = Cv2.ArcLength(p, true);
+                    if (length > 100)
+                    {
+                        new_contours.Add(p);
+                    }
+                }
+
+                p1X = xList.Min();
+                p2X = xList.Max();
+                p1Y = yList.Min();
+                p2Y = yList.Max();
+            }
+            catch
+            {
+                return null;
+            }
+
+            simROIImage.Dispose();
+            return (new Point(p1X, p1Y), new Point(p2X, p2Y));
+        }
+
+        public static (Point, Point)? ExtractCoordinatesMaskImage(Mat simRImage, out Mat? outMat)
+        {
+            //Cv2.PyrDown(simRImage, simRImage);
+
+            Mat simROIImage = new();
+            Cv2.InRange(simRImage, new Scalar(0, 0, 200), new Scalar(50, 50, 255), simROIImage);
+            Cv2.Threshold(simROIImage, simROIImage, 127, 255, ThresholdTypes.Binary);
+
+            Cv2.FindContours(simROIImage, out Point[][] contours, out _, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+            List<int> xList = new();
+            List<int> yList = new();
+            //ImageAssist.SWImage(simROIImage);
+
+            int p1X;
+            int p2X;
+            int p1Y;
+            int p2Y;
+            try
+            {
+                List<Point[]> new_contours = new();
+                foreach (Point[] p in contours)
+                {
+                    foreach (Point p2 in p)
+                    {
+                        xList.Add(p2.X);
+                        yList.Add(p2.Y);
+                    }
+
+                    double length = Cv2.ArcLength(p, true);
+                    if (length > 100)
+                    {
+                        new_contours.Add(p);
+                    }
+                }
+
+                p1X = xList.Min();
+                p2X = xList.Max();
+                p1Y = yList.Min();
+                p2Y = yList.Max();
+            }
+            catch
+            {
+                outMat = null;
+                return null;
+            }
+
+            outMat = simRImage.Clone();
+            Cv2.Rectangle(outMat, new Point(p1X, p1Y), new Point(p2X, p2Y), Scalar.GreenYellow, 2, LineTypes.AntiAlias);
+
+            simROIImage.Dispose();
+
+            return (new Point(p1X, p1Y), new Point(p2X, p2Y));
+        }
+    }
+
+    
+}
