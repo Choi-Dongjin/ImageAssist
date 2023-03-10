e8fb52dc1db06ac56eb0e281f6bbd8f4a342f938
KongGuRE <dongjin0316@gmail.com>
2023-03-10 14:03:54 +0900

    feat: 이미지 기본 정보 불러오는 기능 추가
    X, Y, Channel, BitDepth 검색

---
+++ ImageInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using OpenCvSharp;
+
+namespace ImageAssist
+{
+    public class ImageInfo
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int ChannelCount { get; private set; }
+        public int BitDepth { get; private set; }
+
+        public string FileName { get; init; }
+
+        public ImageInfo(string fileName)
+        {
+            this.FileName = fileName;
+        }
+
+        public void GetImageInfoType1()
+        {
+            using (var mat = new Mat(this.FileName))
+            {
+                Width = mat.Width;
+                Height = mat.Height;
+                ChannelCount = mat.Channels();
+                BitDepth = mat.ElemSize1() * 8;
+            }
+        }
+
+        public void GetImageInfoType2()
+        {
+            using (var image = Image.FromFile(this.FileName))
+            {
+                Width = image.Width;
+                Height = image.Height;
+                Bitmap bitmap = new Bitmap(image);
+                ChannelCount = bitmap.PixelFormat.ToString().Contains("Indexed") ? 1 : Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+                BitDepth = Image.GetPixelFormatSize(bitmap.PixelFormat);
+            }
+        }
+    }
+}
